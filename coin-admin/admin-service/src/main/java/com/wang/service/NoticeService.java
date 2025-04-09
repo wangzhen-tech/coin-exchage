bcd8f33d80fe88ba74886b49db90d8cd370501eb
wangzhen <wangzhen_coder@163.com>
2025-04-09 19:24:09 +0800

    feat:Admin-service:网站运营-> 公告管理功能实现

--- coin-admin/admin-service/src/main/java/com/wang/service/NoticeService.java
+++ coin-admin/admin-service/src/main/java/com/wang/service/NoticeService.java
@@ -2,7 +2,8 @@ package com.wang.service;
 
 import com.wang.domain.Notice;
 import com.baomidou.mybatisplus.extension.service.IService;
-    /**
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+/**
  * @Author wangzhen
  * @Description ${description}
  * @Date 2025/4/3 16:11
@@ -10,5 +11,22 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface NoticeService extends IService<Notice>{
 
+    /**
+     * 条件查询公告
+     * @param page 分页参数
+     * @param title 公告的标签
+     * @param startTime 公告创建的开始时间
+     * @param endTime 公告创建的结束时间
+     * @param status 公告当前的状态
+     * @return
+     */
+    Page<Notice> findByPage(Page<Notice> page, String title, String startTime, String endTime, Integer status);
 
+
+    /**
+     * 查询公告
+     * @param page
+     * @return
+     */
+    Page<Notice> findNoticeForSimple(Page<Notice> page);
 }
--- coin-admin/admin-service/src/main/java/com/wang/service/impl/NoticeServiceImpl.java
+++ coin-admin/admin-service/src/main/java/com/wang/service/impl/NoticeServiceImpl.java
@@ -1,12 +1,14 @@
 package com.wang.service.impl;
 
 import org.springframework.stereotype.Service;
-import javax.annotation.Resource;
-import java.util.List;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.wang.mapper.NoticeMapper;
 import com.wang.domain.Notice;
 import com.wang.service.NoticeService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.util.StringUtils;
+
 /**
  * @Author wangzhen
  * @Description ${description}
@@ -15,5 +17,38 @@ import com.wang.service.NoticeService;
  */
 @Service
 public class NoticeServiceImpl extends ServiceImpl<NoticeMapper, Notice> implements NoticeService{
+
+    /**
+     * 条件查询公告
+     *
+     * @param page      分页参数
+     * @param title     公告的标签
+     * @param startTime 公告创建的开始时间
+     * @param endTime   公告创建的结束时间
+     * @param status    公告当前的状态
+     * @return
+     */
+    @Override
+    public Page<Notice> findByPage(Page<Notice> page, String title, String startTime, String endTime, Integer status) {
+        return page(page,new LambdaQueryWrapper<Notice>()
+                .like(!StringUtils.isEmpty(title),Notice::getTitle,title)
+                .between(!StringUtils.isEmpty(startTime)&&!StringUtils.isEmpty(endTime) ,Notice::getCreated,startTime,endTime+" 23:59:59")
+                .eq(status!=null,Notice::getStatus,status)
+        );
+    }
+
+    /**
+     * 查询公告
+     *
+     * @param page
+     * @return
+     */
+    @Override
+    public Page<Notice> findNoticeForSimple(Page<Notice> page) {
+        return page(page,new LambdaQueryWrapper<Notice>()
+                .eq(Notice::getStatus,1)
+                .orderByAsc(Notice::getSort)
+        );
+    }
 
 }
