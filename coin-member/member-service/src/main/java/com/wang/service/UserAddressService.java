cfac3e391eb99f889405c2c894479196a3a5dc50
wangzhen <wangzhen_coder@163.com>
2025-04-10 17:33:48 +0800

    feat:后台管理系统-用户中心-会员管理-用户详情-提币

--- coin-member/member-service/src/main/java/com/wang/service/UserAddressService.java
+++ coin-member/member-service/src/main/java/com/wang/service/UserAddressService.java
@@ -3,8 +3,6 @@ package com.wang.service;
 import com.wang.domain.UserAddress;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.wang.domain.UserAddress;
-import com.baomidou.mybatisplus.extension.service.IService;
 import java.util.List;
 
 /**
@@ -16,10 +14,8 @@ import java.util.List;
 public interface UserAddressService extends IService<UserAddress>{
     /**
      * 通过用户的Id 分页查询用户的钱包地址
-     * @param page
-     * 分页参数
-     * @param userId
-     * 用户的Id
+     * @param page 分页参数
+     * @param userId 用户的Id
      * @return
      */
     Page<UserAddress> findByPage(Page<UserAddress> page, Long userId);
--- coin-member/member-service/src/main/java/com/wang/service/UserWalletService.java
+++ coin-member/member-service/src/main/java/com/wang/service/UserWalletService.java
@@ -2,13 +2,38 @@ package com.wang.service;
 
 import com.wang.domain.UserWallet;
 import com.baomidou.mybatisplus.extension.service.IService;
-    /**
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.wang.domain.UserWallet;
+import com.baomidou.mybatisplus.extension.service.IService;
+import java.util.List;
+/**
  * @Author wangzhen
  * @Description ${description}
  * @Date 2025/4/10 14:48
  * @Version 1.0
  */
 public interface UserWalletService extends IService<UserWallet>{
+    /**
+     * 分页查询用户的提币地址
+     * @param page 分页参数
+     * @param userId  用户的ID
+     * @return
+     */
+    Page<UserWallet> findByPage(Page<UserWallet> page, Long userId);
 
+    /**
+     * 查询用户的提币的地址
+     * @param userId 用户的Id
+     * @param coinId 币种的Id
+     * @return
+     */
+    List<UserWallet> findUserWallets(Long userId, Long coinId);
 
+    /**
+     * 删除用户的提现地址
+     * @param addressId 提现地址的Id
+     * @param payPassword 交易密码
+     * @return
+     */
+    boolean deleteUserWallet(Long addressId, String payPassword);
 }
