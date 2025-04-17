91ac433455028e0bfbadcb3177f502bdf2858ac0
Arikan-y7000 <wangzhen_coder@163.com>
2025-04-17 16:58:50 +0800

    feat:后台系统-币币交易参数-币种类型配置实现

--- coin-finance/finance-service/src/main/java/com/wang/service/CoinTypeService.java
+++ coin-finance/finance-service/src/main/java/com/wang/service/CoinTypeService.java
@@ -2,13 +2,30 @@ package com.wang.service;
 
 import com.wang.domain.CoinType;
 import com.baomidou.mybatisplus.extension.service.IService;
-    /**
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.wang.domain.CoinType;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
+/**
  * @Author wangzhen
  * @Description ${description}
  * @Date 2025/4/17 15:52
  * @Version 1.0
  */
-public interface CoinTypeService extends IService<CoinType>{
+public interface CoinTypeService extends IService<CoinType> {
+    /**
+     * 条件分页查询货币类型
+     * @param page  分页参数
+     * @param code 币种类型的Code
+     * @return 分页货币类型
+     */
+    Page<CoinType> findByPage(Page<CoinType> page, String code);
 
-
+    /**
+     * 使用币种类型的状态查询所有的币种类型值
+     * @param status
+     * @return
+     */
+    List<CoinType> listByStatus(Byte status);
 }
--- coin-finance/finance-service/src/main/java/com/wang/service/impl/CoinTypeServiceImpl.java
+++ coin-finance/finance-service/src/main/java/com/wang/service/impl/CoinTypeServiceImpl.java
@@ -1,12 +1,14 @@
 package com.wang.service.impl;
 
 import org.springframework.stereotype.Service;
-import javax.annotation.Resource;
 import java.util.List;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.wang.domain.CoinType;
 import com.wang.mapper.CoinTypeMapper;
 import com.wang.service.CoinTypeService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.util.StringUtils;
 /**
  * @Author wangzhen
  * @Description ${description}
@@ -14,6 +16,30 @@ import com.wang.service.CoinTypeService;
  * @Version 1.0
  */
 @Service
-public class CoinTypeServiceImpl extends ServiceImpl<CoinTypeMapper, CoinType> implements CoinTypeService{
+public class CoinTypeServiceImpl extends ServiceImpl<CoinTypeMapper, CoinType> implements CoinTypeService {
 
+    /**
+     * 条件分页查询货币类型
+     *
+     * @param page 分页参数
+     * @param code 类型的Code
+     * @return 分页货币类型
+     */
+    @Override
+    public Page<CoinType> findByPage(Page<CoinType> page, String code) {
+        Page<CoinType> pageData = page(page, new LambdaQueryWrapper<CoinType>()
+                .like(!StringUtils.isEmpty(code), CoinType::getCode, code));
+        return pageData;
+    }
+
+    /**
+     * 使用币种类型的状态查询所有的币种类型值
+     *
+     * @param status
+     * @return
+     */
+    @Override
+    public List<CoinType> listByStatus(Byte status) {
+        return list(new LambdaQueryWrapper<CoinType>().eq(status!=null ,CoinType::getStatus,status));
+    }
 }
