af412cce37f240b1531dc94fc6fd257b268a6735
Arikan-y7000 <wangzhen_coder@163.com>
2025-04-18 11:16:34 +0800

    feat:后台系统-资金明细-充提记录-提币记录

--- coin-finance/finance-service/src/main/java/com/wang/domain/CoinWithdraw.java
+++ coin-finance/finance-service/src/main/java/com/wang/domain/CoinWithdraw.java
@@ -14,14 +14,11 @@ import lombok.NoArgsConstructor;
 
 /**
  * @Author wangzhen
- * @Description ${description}
+ * @Description 数字货币提现记录
  * @Date 2025/4/17 15:52
  * @Version 1.0
  */
-/**
-    * 数字货币提现记录
-    */
-@ApiModel(description="数字货币提现记录")
+@ApiModel(value = "com-wang-domain-CoinWithdraw")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -30,140 +27,150 @@ public class CoinWithdraw {
     /**
      * 自增id
      */
-    @TableId(value = "id", type = IdType.INPUT)
-    @ApiModelProperty(value="自增id")
+    @TableId(value = "id", type = IdType.AUTO)
+    @ApiModelProperty(value = "自增id")
     private Long id;
 
     /**
      * 用户id
      */
     @TableField(value = "user_id")
-    @ApiModelProperty(value="用户id")
+    @ApiModelProperty(value = "用户id")
     private Long userId;
 
     /**
      * 币种id
      */
     @TableField(value = "coin_id")
-    @ApiModelProperty(value="币种id")
+    @ApiModelProperty(value = "币种id")
     private Long coinId;
 
     /**
      * 币种名称
      */
     @TableField(value = "coin_name")
-    @ApiModelProperty(value="币种名称")
+    @ApiModelProperty(value = "币种名称")
     private String coinName;
 
     /**
      * 币种类型
      */
     @TableField(value = "coin_type")
-    @ApiModelProperty(value="币种类型")
+    @ApiModelProperty(value = "币种类型")
     private String coinType;
 
     /**
      * 钱包地址
      */
     @TableField(value = "address")
-    @ApiModelProperty(value="钱包地址")
+    @ApiModelProperty(value = "钱包地址")
     private String address;
 
     /**
      * 交易id
      */
     @TableField(value = "txid")
-    @ApiModelProperty(value="交易id")
+    @ApiModelProperty(value = "交易id")
     private String txid;
 
     /**
      * 提现量
      */
     @TableField(value = "num")
-    @ApiModelProperty(value="提现量")
+    @ApiModelProperty(value = "提现量")
     private BigDecimal num;
 
     /**
      * 手续费()
      */
     @TableField(value = "fee")
-    @ApiModelProperty(value="手续费()")
+    @ApiModelProperty(value = "手续费()")
     private BigDecimal fee;
 
     /**
      * 实际提现
      */
     @TableField(value = "mum")
-    @ApiModelProperty(value="实际提现")
+    @ApiModelProperty(value = "实际提现")
     private BigDecimal mum;
 
     /**
      * 0站内1其他2手工提币
      */
-    @TableField(value = "`type`")
-    @ApiModelProperty(value="0站内1其他2手工提币")
+    @TableField(value = "type")
+    @ApiModelProperty(value = "0站内1其他2手工提币")
     private Boolean type;
 
     /**
      * 链上手续费花费
      */
     @TableField(value = "chain_fee")
-    @ApiModelProperty(value="链上手续费花费")
+    @ApiModelProperty(value = "链上手续费花费")
     private BigDecimal chainFee;
 
     /**
      * 区块高度
      */
     @TableField(value = "block_num")
-    @ApiModelProperty(value="区块高度")
+    @ApiModelProperty(value = "区块高度")
     private Integer blockNum;
 
     /**
      * 后台审核人员提币备注备注
      */
     @TableField(value = "remark")
-    @ApiModelProperty(value="后台审核人员提币备注备注")
+    @ApiModelProperty(value = "后台审核人员提币备注备注")
     private String remark;
 
     /**
      * 钱包提币备注备注
      */
     @TableField(value = "wallet_mark")
-    @ApiModelProperty(value="钱包提币备注备注")
+    @ApiModelProperty(value = "钱包提币备注备注")
     private String walletMark;
 
     /**
      * 当前审核级数
      */
     @TableField(value = "step")
-    @ApiModelProperty(value="当前审核级数")
+    @ApiModelProperty(value = "当前审核级数")
     private Byte step;
 
     /**
      * 状态：0-审核中；1-成功；2-拒绝；3-撤销；4-审核通过；5-打币中；
      */
-    @TableField(value = "`status`")
-    @ApiModelProperty(value="状态：0-审核中；1-成功；2-拒绝；3-撤销；4-审核通过；5-打币中；")
+    @TableField(value = "status")
+    @ApiModelProperty(value = "状态：0-审核中；1-成功；2-拒绝；3-撤销；4-审核通过；5-打币中；")
     private Boolean status;
 
     /**
      * 审核时间
      */
     @TableField(value = "audit_time")
-    @ApiModelProperty(value="审核时间")
+    @ApiModelProperty(value = "审核时间")
     private Date auditTime;
 
     /**
      * 修改时间
      */
     @TableField(value = "last_update_time")
-    @ApiModelProperty(value="修改时间")
+    @ApiModelProperty(value = "修改时间")
     private Date lastUpdateTime;
 
     /**
      * 创建时间
      */
     @TableField(value = "created")
-    @ApiModelProperty(value="创建时间")
+    @ApiModelProperty(value = "创建时间")
     private Date created;
+
+
+    @TableField(exist = false)
+    @ApiModelProperty(value = "用户的名称")
+    private String username ;
+
+    @TableField(exist = false)
+    @ApiModelProperty(value = "用户的真实名称")
+    private String realName ;
+
 }
